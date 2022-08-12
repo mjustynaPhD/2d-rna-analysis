0c9b85b39aeac90336c1d10843b9afa88ef60649
Marek Justyna <mjustyna32@gmail.com>
2022-08-12 16:04:39 +0000

    Filtering by representants

--- rna2d/pipelines/pipeline.py
+++ rna2d/pipelines/pipeline.py
@@ -1,15 +1,21 @@
+from dataclasses import dataclass
+from typing import Dict, Tuple
 from rna2d.utils import *
 
-class Pipeline():
-    def __init__(self, *args, **kwargs):
-        pass
 
-    def run_for_all(self, out_path:str, results_path:str):
-        res = get_results(out_path=out_path, res_path=results_path)
+@dataclass
+class Pipeline():
+    out_path: str
+    results_path: str
+
+    def run_for_all(self) -> Tuple[Dict, Dict]:
+        res = get_results(out_path=self.out_path, res_path=self.results_path)
         ids = get_subset_ids(res)
-        common = filter_common(ids)        
+        common = filter_common(ids)
         methods_results, indeces = collect_results(res, common, ids)
         return methods_results, indeces
 
-    def run_filtered(self):
+    def run_filtered(self, filter_ids_path: str):
+        methods_results, indeces = self.run_for_all()
+
         pass
