feecf53b2cbe2a86415215636f04145ccc2313ca
Marek Justyna <mjustyna@cs.put.poznan.pl>
2023-04-03 17:16:48 +0000

    Comment updated

--- scripts/preprocessing/clean-non0.sh
+++ scripts/preprocessing/clean-non0.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
+# Remove structures with no base pairs
+
 for f in $1/*
 do
     
     if [ -d $f ]
     then
         # echo $f
-        res=`cat $f/Target/*.bpseq | awk '{print $3;}' | sort | uniq | wc -l `
+        res=`cat $f/0/whole.bpseq | awk '{print $3;}' | sort | uniq | wc -l `
 
         
         if [ "$res" -eq "1" ]
         then
-            echo $f
+            echo $f $res
             sudo rm -rf $f
         fi
     
